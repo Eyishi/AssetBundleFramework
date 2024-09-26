b93aeab0db0b5a437215bb00fb623ec7e198a5e3
Eyishi <15922751294@163.com>
2024-09-26 17:32:55 +0800

    增加切换平台和加载配置流程

---
+++ Assets/AssetBundleFramework/Editor/BuildSetting.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace AssetBundleFramework.Editor
+{
+    /// <summary>
+    /// 构建平台
+    /// </summary>
+    public class BuildSetting : ISupportInitialize
+    {
+        
+    }
+}
--- Assets/AssetBundleFramework/Editor/Builder.cs
+++ Assets/AssetBundleFramework/Editor/Builder.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using AssetBundleFramework.Core;
+using AssetBundleFramework.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,6 +13,30 @@ using UnityEngine;
 /// </summary>
 public class Builder : MonoBehaviour
 {
+    
+    private static readonly Profiler ms_BuildProfiler = new Profiler(nameof(Builder));
+    /// <summary>
+    /// 配置
+    /// </summary>
+    private static readonly Profiler ms_LoadBuildSettingProfiler = ms_BuildProfiler.CreateChild(nameof(LoadSetting));
+    /// <summary>
+    /// 转换平台
+    /// </summary>
+    private static readonly Profiler ms_SwitchPlatformProfiler = ms_BuildProfiler.CreateChild(nameof(SwitchPlatform));
+    
+#if UNITY_IOS
+        private const string PLATFORM = "iOS";
+#elif UNITY_ANDROID
+        private const string PLATFORM = "Android";
+#else
+    private const string PLATFORM = "Windows";
+#endif
+    
+    /// <summary>
+    /// 打包设置
+    /// </summary>
+    public static BuildSetting buildSetting { get; private set; }
+    
     #region Build MenuItem
 
     [MenuItem("Tools/ResBuild/Windows")]
@@ -28,6 +57,54 @@ public class Builder : MonoBehaviour
         Build();
     }
 
+    /// <summary>
+    /// 切换打包平台
+    /// </summary>
+    public static void SwitchPlatform()
+    {
+        string platform = PLATFORM;
+        
+        //切换平台
+        switch (platform)
+        {
+            case "windows":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, 
+                    BuildTarget.StandaloneWindows64);
+                break;
+            case "android":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, 
+                    BuildTarget.Android);
+                break;
+            case "ios":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, 
+                    BuildTarget.iOS);
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 加载打包配置文件
+    /// </summary>
+    /// <param name="settingPath">打包配置路径</param>
+    private static BuildSetting LoadSetting(string settingPath)
+    {
+        buildSetting = XmlUtility.Read<BuildSetting>(settingPath);
+        if (buildSetting == null)
+        {
+            throw new Exception($"Load buildSetting failed,SettingPath:{settingPath}.");
+        }
+        (buildSetting as ISupportInitialize)?.EndInit();
+
+        buildPath = Path.GetFullPath(buildSetting.buildRoot).Replace("\\", "/");
+        if (buildPath.Length > 0 && buildPath[buildPath.Length - 1] != '/')
+        {
+            buildPath += "/";
+        }
+        buildPath += $"{PLATFORM}/";
+
+        return buildSetting;
+    }
+    
     #endregion
     
     
